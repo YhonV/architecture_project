19dcccc47494c3137b3ac2adc2fe6c01434b8bc5
YhonVivas <yohnvivas16@gmail.com>
2024-10-29 23:00:43 -0300

    mejoras a la navbar

--- web_project/views.py
+++ web_project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
