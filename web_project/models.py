86ced2186cd7b07ba261828bc06b6659a414947f
YhonVivas <yohnvivas16@gmail.com>
2024-11-16 15:34:59 -0300

    Añado sqlite

--- web_project/models.py
+++ web_project/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Cliente (models.Model):
+    id_cliente = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    nombre_completo = models.CharField(max_length=100)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre_completo
