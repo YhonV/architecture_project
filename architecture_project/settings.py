de9aff970b6bdfa95858ff1414a561f886cf6a70
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-12-03 20:07:35 -0300

    agrego postgreSQL

--- architecture_project/settings.py
+++ architecture_project/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'architecture_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'compraAhorra',
+        'USER': 'postgres',
+        'PASSWORD': 'compra123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
